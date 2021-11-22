43674d8139a0be1bd40216f3951cf47acddb5b97
Simon Sjöling <rizedualsub@gmail.com>
2021-11-22 16:37:04 +0100

    Potentiell lösning till U4. d.) i U4_d_2.m

--- 2A/U4_d_2.m
+++ 2A/U4_d_2.m
@@ -1,37 +1,31 @@
 clc, clear
 
 D = 1;
+N = 100;
 L = 1.2;
+dx = L/N;
 dt = 10^-4;
 tspan = [0, 0.03];
+xv = (0:dx:L);
+xvinner = (dx:dx:L-dx);
 
+uv0 = sin(4*pi.*xvinner)';
 dir1 = @(t) 0.* t;
 dir2 = @(t)(sin(4*pi*L)*exp(-16*(pi^2) .* t));
+
+[A, s] = VLE_rums_diskreting(N-1, D, L, dir1, dir2);
+
+gfunc = @(t) (D/(dx^2))* s(t);
 
 % Beräknar tidsvektor
 tend = 0.03;
 h = (1/(2^4)) * dt; % Först för det minsta värdet på alpha
 tv = h*(0:floor(tend/h)); % Beräknar alla tider i tidsspannet.
 
+Uana = sin(4*pi .* xv)'*exp(-16*pi^2 .* tv);
+
 pvec = zeros(1, 5);
 for n=0:4
-    N = 100 * 2^n;
-    dx = L/N;
-    xv = (0:dx:L);
-    xvinner = (dx:dx:L-dx);
-    
-    M = N*10; 
-    dt = (tspan(2) - tspan(1))/M;
-    tv = (tspan(1):dt:tspan(2));
-    
-    uv0 = sin(4*pi.*xvinner)';
-  
-    Uana = sin(4*pi .* xv)'*exp(-16*pi^2 .* tv);
-    
-    [A, s] = VLE_rums_diskreting(N-1, D, L, dir1, dir2);
-
-    gfunc = @(t) (D/(dx^2))* s(t);
-
     tvn = tv(1:2^n:end);
     [~, solminner] = ITM((D/(dx^2))*A, gfunc, tvn, uv0);
     res = dir1(tvn);
@@ -39,9 +33,9 @@ for n=0:4
     diff = solm(2,:) - Uana(2,1:2^n:end);
     err = norm2(solm(:,end), Uana(:,end));
     pvec(n+1) = err; % Fyller backlänges.
-    
-    fprintf("Tidssteg: dt=%d, M=%d Rumssteg: dx=%d, N=%d ger felet: %d \n", dt, M, dx, N, err);
 end
+
+display(pvec);
 
 % Beräknar noggrannhetsordning
 pe10 = log(pvec(1) / pvec(2))/log(2);
